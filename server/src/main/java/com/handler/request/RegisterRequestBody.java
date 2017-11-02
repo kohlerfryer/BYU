803b0a372d3009c575b1fd333999210413f0c816
David Pack <dpack3.0@gmail.com>
2017-11-02 13:56:53 -0600

    ...

--- server/src/main/java/com/handler/request/RegisterRequestBody.java
+++ server/src/main/java/com/handler/request/RegisterRequestBody.java
@@ -1,30 +1,33 @@
-package com.familymap;
+//  LITTLE TOO OP FOR THE TIME FRAME//
 
-public interface RequestBody{
-    private validateInputs();
-}
+
+// package com.familymap;
+
+// public interface RequestBody{
+//     private validateInputs();
+// }
 
 public class RegisterRequestBody implements RequestBody{
-    private String userName;
-    private String password;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String gender;
+    // private String userName;
+    // private String password;
+    // private String email;
+    // private String firstName;
+    // private String lastName;
+    // private String gender;
 
-    public RegisterRequestBody(String userName, String password, String email, String firstName, String lastName, String gender){
-        this.userName = userName;
-        this.password = password;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-    }
+    // public RegisterRequestBody(String userName, String password, String email, String firstName, String lastName, String gender){
+    //     this.userName = userName;
+    //     this.password = password;
+    //     this.email = email;
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    //     this.gender = gender;
+    // }
 
-    private validateInputs() throws InvalidRequestException{
-        //throw new InvalidRequestExpection("invalid parameters");
-        //gson.toJson(dobj, new FileWriter("D:\\file.json"));
-    }
+    // private validateInputs() throws InvalidRequestException{
+    //     //throw new InvalidRequestExpection("invalid parameters");
+    //     //gson.toJson(dobj, new FileWriter("D:\\file.json"));
+    // }
 
 
 }
